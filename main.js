ea832f4670f4ca9cd3bb2de1050062762cd8c100
Samuel Weke <wekesamuel@yahoo.com>
2019-12-05 12:51:48 +0100

    final

--- main.js
+++ main.js
@@ -10,26 +10,26 @@ const next = document.querySelector('#next');
 const previous = document.querySelector('#previous');
 let page = 1; 
 
-previous.style.display = 'none'
+previous.style.display = 'none';
 
 previous.addEventListener('click', () =>{
     page -= 1;
     displayCoins(page);
-    next.style.display = 'block'
+    next.style.display = 'block';
     if(page == 1){
-      previous.style.display = 'none'
-    }
-})
+      previous.style.display = 'none';
+    };
+});
 
 next.addEventListener ('click', () => {
   previous.style.display= 'block';
   page += 1;
   displayCoins(page);
-
   if (page === 5) {
       next.style.display = 'none';
-  }
-})
+  };
+});
+
 const getCoins = async () => {
   const response = await fetch('https://api.coinlore.com/api/tickers/?start=0&limit=50');
   const data = await response.json();
@@ -38,19 +38,20 @@ const getCoins = async () => {
 }
 
 const displayCoins = (page) => {
-    tbody.innerHTML = "";
-    const rowPerPage = 10;
-    const begin = (page * rowPerPage) - rowPerPage;
-    const end = page * rowPerPage;
-    for (let i = begin; i < end; i+= 1) {
-        let tr = document.createElement("tr");
-        tr.innerHTML = `
-          <td data-title="💰 Coin">${coin[i].name}</td>
-          <td data-title="📄 Code">${coin[i].symbol}</td>
-          <td data-title="🤑 Price">&dollar; ${coin[i].price_usd}</td>
-          <td data-title="📉 Total Supply">${coin[i].tsupply}</td>`;
-        tbody.appendChild(tr)
-    }
-}
+  tbody.innerHTML = "";
+  const begin = (page * 10) - 10;
+  const end = page * 10;
+  for (let i = begin; i < end; i++) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td data-title="💰 Coin">${coin[i].name}</td>
+      <td data-title="📄 Code">${coin[i].symbol}</td>
+      <td data-title="🤑 Price">&dollar; ${coin[i].price_usd}</td>
+      <td data-title="📉 Total Supply">${coin[i].tsupply}</td> 
+    `;
+    tbody.appendChild(tr);
+  };
+};
+
 getCoins();
 
